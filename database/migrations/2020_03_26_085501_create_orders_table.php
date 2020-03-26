d687f42f99e879b1439969b33bb5da0ecde4be8a
sunil178 <sunilthakur178co@gmail.com>
2020-03-26 14:55:10 +0530

    Razor Pay

---
+++ database/migrations/2020_03_26_085501_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contact_number')->nullable();
+            $table->string('amount')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->string('order_id')->nullable();
+            $table->string('payment_id')->nullable();
+            $table->string('pancard')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
